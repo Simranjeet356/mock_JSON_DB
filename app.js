3f9170048b49d9115e1b148a8dcfe38ad6ffd16e
Simranjeet356 <simranjeetkaur.lnu8@rdpolytech.ca>
2025-03-19 18:47:31 -0600

    Update

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require ('express');
+const app = express();
+require ('dotenv').config();
+const PORT = process.env.PORT;
+
+
+//Setup the view engine
+app.set('view engine','ejs');
+app.set('view', './views')
+
+
+// Middleware
+app.use((req, res, next)=>{
+    const time = new Date();
+    console.log(time, req.url,req.method);
+    next();
+});
+app.get('/home',(req,res) =>{
+  console.log('This is always trigged secondly');
+});
+
+
+app.listen(PORT,()=>{
+    console.log(`Connected on port:${PORT}`)
+}
+);
+

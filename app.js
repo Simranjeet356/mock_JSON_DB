42c8424c45151f854562eb05381cd19fd344ac70
Simranjeet356 <simranjeetkaur.lnu8@rdpolytech.ca>
2025-03-20 18:23:37 -0600

    update

--- app.js
+++ app.js
@@ -9,16 +9,8 @@ app.set('view engine','ejs');
 app.set('view', './views')
 
 
-// Middleware
-app.use((req, res, next)=>{
-    const time = new Date();
-    console.log(time, req.url,req.method);
-    next();
-}); 
-app.get('/home',(req,res) =>{
-  console.log('This is always trigged secondly');
-});
 
+app.use(express.static('public'));
 
 app.listen(PORT,()=>{
     console.log(`Connected on port:${PORT}`)

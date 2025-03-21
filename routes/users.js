8ba7fbab68d131c0871dacf79a25d3fc4dad3262
Simranjeet356 <simranjeetkaur.lnu8@rdpolytech.ca>
2025-03-20 18:58:14 -0600

    Update

---
+++ routes/users.js
@@ -0,0 +1,20 @@
+const express =  require('express');
+const router = express.Router();
+
+
+//Router handler for our home page
+router.get('/home', (req, res)=>{
+    res.render ('home');
+}
+)
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
